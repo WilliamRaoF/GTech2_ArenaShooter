dfafaf8d16eb7f8514e6b7e047be0cf18ed649b8
William Rao Fernandes <williamraofernandes@gmail.com>
2025-12-05 12:45:00 +0100

    Create HealthComponent.h

---
+++ HealthComponent.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "HealthComponent.generated.h"
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class ARENASHOOTER_API UHealthComponent : public UActorComponent
+{
+    GENERATED_BODY()
+
+public:
+    UHealthComponent();
+
+protected:
+    virtual void BeginPlay() override;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Health")
+    float MaxHealth = 100.0f;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Health")
+    float CurrentHealth;
+
+public:
+    UFUNCTION(BlueprintCallable, Category="Health")
+    void TakeDamage(float Amount);
+
+    UFUNCTION(BlueprintCallable, Category="Health")
+    bool IsDead() const;
+};
